45e7b3ad1eaf3f4cc6a2a0700a90a983db131849
Flowmaster <ellyssin.gimhae@gmail.com>
2016-12-26 01:30:39 -0700

    Added SportsBet scraper.

--- sportsbet.py
+++ sportsbet.py
@@ -1,24 +1,147 @@
 import requests
 import re
 import mechanize
-from BeautifulSoup import BeautifulSoup
+import unittest
+import json
+import urllib2
+import time
 
-url = 'https://sportsbet.io/sports'
-url = 'https://www.cloudbet.com/en/sports'
+from bs4 import BeautifulSoup
+from datetime import datetime, timedelta
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException
 
-browser = mechanize.Browser()
-browser.set_handle_equiv(True)
-browser.set_handle_redirect(True)
-browser.set_handle_referer(True)
-browser.set_handle_robots(False)
-browser.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
 
-browser.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
-response = browser.open(url)
+class SportsBet(unittest.TestCase):
+    def setUp(self):
+        # link to NBA basketball events
+        self.sport = "NBA Basketball"
+        self.sport = "Basketball NBA"
+        url = 'https://sportsbet.io/sports'
 
-soup = BeautifulSoup(response.read())
-#links = soup.findAll("a")
+        self.driver = webdriver.PhantomJS()
+        self.driver.set_window_size(1120, 550)
+        self.driver.implicitly_wait(10)
+        self.driver.get(url)
 
-for link in browser.links():
-    print link.text, link.url
-#print thing
+    def check_exists_by_class(self, classname):
+        try:
+            self.driver.find_element_by_class_name(classname)
+        except NoSuchElementException:
+            return False
+        return True
+
+    def check_exists_by_xpath(self, path):
+        try:
+            self.driver.find_element_by_xpath(path)
+        except NoSuchElementException:
+            return False
+        return True
+
+    def test_main(self):
+        driver = self.driver
+        formatter1 = '%d %b %y'
+        formatter2 = '%d.%m.%Y'
+        formatter3 = '%d.%m.%Y %H:%M'
+        formatter4 = '%a, %b %d, %Y %I:%M %p'
+
+        sportPath = "//*[text() = '"+self.sport+"']"
+        sportElement = driver.find_element_by_xpath(sportPath)
+        sportElement.click()
+
+        dates = driver.find_element_by_class_name("date-badges-container")
+        page = BeautifulSoup(driver.page_source, "html.parser")
+
+        allEvents = []
+        dateElements = page.find_all("div", {"class": "date-badge"})
+        for element in dateElements:
+            date = element.get_text().split(" ", 1)[1]
+
+            if (date == "Matches"):
+                continue
+
+            datePath = "//*[text() = '"+date+"']"
+            dateObj = driver.find_element_by_xpath(datePath)
+            dateObj.click()
+
+            formattedTime1 = datetime.strptime(date, formatter1)
+            date = formattedTime1.strftime(formatter2)
+            datePath2 = "//*[text() = '"+date+" ']"
+
+            if self.check_exists_by_xpath(datePath2) is not True:
+                continue
+            else:
+                page2 = BeautifulSoup(driver.page_source, "html.parser")
+                events = page2.find_all("div", {"class":"event"})
+                for evt in events:
+                    content = evt.getText()
+                    if "Markets will be available soon" in content:
+                        continue
+
+                    t = evt.find("div", {"class":"start"})
+                    description = evt.find("div", {"class":"description"})
+                    if (description == None):
+                        continue
+
+                    t = t.getText().split("Starts")[0]
+                    t = ' '.join(t.split())
+                    t = datetime.strptime(t, formatter3)
+                    name = description.find("b").string
+
+                    detailPath = "//*[text() = '"+name+"']"
+                    detailElement = driver.find_element_by_xpath(detailPath)
+                    detailElement.click()
+
+                    linePath = "//*[text() = 'Points Spreads']"
+                    spreads = WebDriverWait(driver, 10).until( lambda driver: driver.find_element_by_xpath(linePath))
+                    time.sleep(2)
+
+                    # extract spreads here
+                    page3 = BeautifulSoup(driver.page_source, "html.parser")
+
+                    sections = ["Match Winner", "Total Spreads", "Points Spreads"]
+                    eventOptions = []
+                    for section in sections:
+
+                        optionDiv = page3.find(string = section).parent.parent.parent
+                        options = optionDiv.find_all("div", {"class":"cell"}) if (section != "Points Spreads") else optionDiv.find_all("a", {"class":"cell"})
+
+                        for option in options:
+                            optionName = option.find("span", {"class":"truncate"}).string
+                            optionName = optionName.lower().title()
+                            optionName = optionName + " ML" if (section == "Match Winner") else optionName
+                            odds = option.find("div", {"class":"odds"})
+
+                            sportsEventOption = {
+                                "name": optionName,
+                                "odds": float(odds.string)
+                            }
+                            eventOptions.append(sportsEventOption)
+
+                    timeStr =  t.strftime(formatter4)
+                    matchName = name.lower().title().replace(" V ", " vs ")
+                    sportsEvent = {
+                        "name": matchName,
+                        "time": timeStr,
+                        "options": eventOptions
+                    }
+                    allEvents.append(sportsEvent)
+
+                    driver.back()
+                    WebDriverWait(driver, 10).until( lambda driver: driver.find_element_by_xpath(datePath2))
+
+        blob = {
+          "bookname": "SportsBet",
+          "sport": self.sport,
+          "events": allEvents
+        }
+
+        response = requests.post('http://localhost:9000/events', json=blob)
+        print response.content
+
+    def tearDown(self):
+        self.driver.quit()
+
+if __name__ == "__main__":
+    unittest.main()
