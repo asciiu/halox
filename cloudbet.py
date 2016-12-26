9fa40c11a6e9ab86e2e9d19c82c5da27bbe87eb5
Flowmaster <ellyssin.gimhae@gmail.com>
2016-12-25 19:37:55 -0700

    Rework scraper to fit test structure. Also incorporated waits into the script to ensure content is ready to be scraped after loading. This version successfully scrapes point spreads for basketball games.

--- cloudbet.py
+++ cloudbet.py
@@ -1,77 +1,83 @@
 import requests
 import re
 import mechanize
-from selenium import webdriver
-from datetime import datetime, timedelta
+import unittest
+
 from bs4 import BeautifulSoup
+from datetime import datetime, timedelta
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
 
-# link to NFL football events
-url = 'https://www.cloudbet.com/en/sports/competition/208'
-# link to NBA basketball events
-url = 'https://www.cloudbet.com/en/sports/competition/143'
+class Cloudbet(unittest.TestCase):
+    def setUp(self):
+        # link to NFL football events
+        url = 'https://www.cloudbet.com/en/sports/competition/208'
+        # link to NBA basketball events
+        url = 'https://www.cloudbet.com/en/sports/competition/143'
 
-browser = mechanize.Browser()
-browser.set_handle_equiv(True)
-browser.set_handle_redirect(True)
-browser.set_handle_referer(True)
-browser.set_handle_robots(False)
-browser.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+        self.driver = webdriver.PhantomJS()
+        self.driver.set_window_size(1120, 550)
+        self.driver.get(url)
 
-browser.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
-response = browser.open(url)
+    def test_main(self):
+        driver = self.driver
+        soup = BeautifulSoup(driver.page_source, "html.parser")
 
-soup = BeautifulSoup(response.read(), "html.parser")
+        table = soup.find("table", { "class" : "sports-line" })
+        if "sports-line_live" in table["class"]:
+            table = table.next_sibling
 
-table = soup.find("table", { "class" : "sports-line" })
-dates = []
-for thead in table.findAll("thead"):
-    header = thead.find('th')
-    date = header.find('div', {'class':'sports-line-title'})
-    dates.append(date.span.string)
+        formatter = '%a, %b %d, %Y %I:%M %p'
+        date = ""
+        for row in table.findAll("tr"):
+            # if row is header row
+            thead = row.find_parent("thead")
+            if (thead != None):
+                div = row.find('div', {'class':'sports-line-title'})
+                date = div.span.string
+                continue
 
-#print dates
+            timeDiv = row.find("div", {"class":"time"})
+            time = timeDiv.span.string
+            fullDate = date + " " + time
 
-formatter = '%a, %b %d, %Y %I:%M %p'
-date = ""
-for row in table.findAll("tr"):
-    # if row is header row
-    thead = row.find_parent("thead")
-    if (thead != None):
-        div = row.find('div', {'class':'sports-line-title'})
-        date = div.span.string
-        continue
+            time = datetime.strptime(fullDate, formatter)
+            # convert to mountain time?
+            #time = time - timedelta(hours=7)
+            timeStr = time.strftime(formatter)
 
-    time = row.find("div", {"class":"time"}).span.string
-    fullDate = date + " " + time
+            # extract the team names
+            description = row.find("td", {"class":"col2"})
+            teamNames = row.findAll("div", {"class":"team-name-item"})
+            names = map(lambda x: x.string, teamNames)
+            #date = datetime.strftime(formatter)
+            #print date
+            print timeStr
+            print names
 
-    datetime = datetime.strptime(fullDate, formatter)
-    # convert to mountain time
-    datetime = datetime - timedelta(hours=7)
+            href = description.a.get("href")
+            gamePath = "(//a[contains(@href, '"+href+"')])"
+            gameElement = driver.find_element_by_xpath(gamePath)
+            gameElement.click()
 
-    description = row.find("td", {"class":"col2"})
-    teamNames = row.findAll("div", {"class":"team-name-item"})
+            linePath = "//*[text() = 'Points Spreads']"
+            spreads = WebDriverWait(driver, 10).until( lambda driver: driver.find_element_by_xpath(linePath))
 
-    # extract date from thead
-    names = map(lambda x: x.string, teamNames)
-    #date = datetime.strftime(formatter)
-    #print date
-    print names
+            spreads.click()
+            page2 = BeautifulSoup(driver.page_source, "html.parser")
+            pointSpreads = page2.find(string = "Points Spreads").parent.parent
+            options = pointSpreads.find_all("div", {"class":"name"})
+            for option in options:
+                optionName = option.string
+                odds = option.next_sibling.get_text()
+                print optionName + " " + odds
 
-    # follow event link
-    response = browser.follow_link(url = description.a.get('href'))
-    details = BeautifulSoup(response, "html.parser")
-    spreads = details.find(string = "Points Spreads").find_parent("div").find_parent("div")
-    btn = spreads.find("i")
-    btn['class'] = 'icon-angle-up up'
-    options = spreads
+            driver.back()
+            WebDriverWait(driver, 10).until( lambda driver: driver.find_element_by_xpath(gamePath))
+            # follow event link
 
-    print options
+    def tearDown(self):
+        self.driver.quit()
 
-    browser.back()
-    #print response
-
-    #cells = row.findAll("td")
-    #print cells
-
-#for link in browser.links():
-#    print link.text, link.url
+if __name__ == "__main__":
+    unittest.main()
